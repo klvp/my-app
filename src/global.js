b0f1718dd46708aecf3a6627b785c4defbec1c0a
Lakshmi_Vara_Prasad Kuna <klvpfeb98@gmail.com>
2022-03-30 01:56:49 +0530

    api changed

--- src/global.js
+++ src/global.js
@@ -1,5 +1,5 @@
 /** @format */
 
 // export const API = "https://61eb17287ec58900177cdba8.mockapi.io";
-// export const API = "https://klvpmovieapi.herokuapp.com";
-export const API = "http://localhost:4000";
+export const API = "https://klvpmovieapi.herokuapp.com";
+// export const API = "http://localhost:4000";
